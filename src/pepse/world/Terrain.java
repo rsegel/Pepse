15f894365331fc4ff66cb6e677278d2c3ab5daa6
roise <roi.segel@mail.huji.ac.il>
2024-07-15 20:27:48 +0300

    terrain

--- src/pepse/world/Terrain.java
+++ src/pepse/world/Terrain.java
@@ -1,12 +1,18 @@
 package pepse.world;
 
+import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+
+import java.util.List;
+import java.awt.Color;
 
 
 
 
 public class Terrain {
     private static final float DEFAULT_GROUND_FACTOR = (float) (2.0/3.0);
+    private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
     static private float groundHeightAtX0;
     public Terrain(Vector2 windowDimensions, int seed){
         groundHeightAtX0 = windowDimensions.y() * DEFAULT_GROUND_FACTOR;
@@ -18,5 +24,18 @@ public class Terrain {
 
     // TODO: implement with perlin noise
     public float groundHeightAt(float x) { return groundHeightAtX0; }
+
+    public List<Block> createInRange(int minX, int maxX) {
+        // create list of a single block
+        Block singleBlock = new Block(
+                // put the block in the top left corner of the screen
+                new Vector2(0, 0),
+                new RectangleRenderable(ColorSupplier.approximateColor(
+                        BASE_GROUND_COLOR))
+        );
+        singleBlock.setTag("ground");
+        return List.of(singleBlock);
+
+    }
 
 }
--- src/pepse/world/daynight/Sun.java
+++ src/pepse/world/daynight/Sun.java
@@ -27,17 +27,17 @@ public class Sun {
         // TODO: sure?
         GameObject sun = new GameObject(
                 new Vector2(windowDimensions.x() / 2,
-                        windowDimensions.y() / 2),
+                        (windowDimensions.y() - Terrain.getGroundHeightAtX0()) / 2),
                 new Vector2(SUN_DIM, SUN_DIM), // TODO: sun's real dims
                 new OvalRenderable(Color.YELLOW)
         );
         sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         sun.setTag("sun");
         Vector2 initialSunCenter = sun.getCenter();
-        float horizon = Terrain.groundHeightAtX(windowDimensions.x() / 2);
+        float horizon = Terrain.getGroundHeightAtX0();
         Vector2 cycleCenter = new Vector2(windowDimensions.x() / 2, horizon);
         new Transition<Float>(sun,
-                (Float angle) -> sun.setCenter((initialSunCenter.subtract(cycleCenter).rotated(angle).add(cycleCenter))),
+                (Float angle) -> sun.setCenter(initialSunCenter.subtract(cycleCenter).rotated(angle).add(cycleCenter)),
                 0f, FULL_CIRCLE,
                 Transition.LINEAR_INTERPOLATOR_FLOAT,
                 cycleLength, Transition.TransitionType.TRANSITION_LOOP, null);
