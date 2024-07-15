ff24a99f6b29f8726b5c19c679091ec12cc6e8fb
roise <roi.segel@mail.huji.ac.il>
2024-07-15 19:28:00 +0300

    night

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -7,16 +7,20 @@ import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
 import pepse.world.Sky;
+import pepse.world.daynight.Night;
 
 public class PepseGameManager extends GameManager {
 
     private static final int skyLayer = 1;
+    private static final int CYCLE_DEFAULT = 30;
 
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
         GameObject sky = Sky.create(windowController.getWindowDimensions());
         gameObjects().addGameObject(sky, skyLayer);
+        GameObject night = Night.create(windowController.getWindowDimensions(), CYCLE_DEFAULT);
+        gameObjects().addGameObject(night, skyLayer);
     }
 
     public static void main(String[] args) {
--- src/pepse/world/daynight/Night.java
+++ src/pepse/world/daynight/Night.java
@@ -2,16 +2,28 @@ package pepse.world.daynight;
 
 import danogl.GameObject;
 import danogl.components.CoordinateSpace;
+import danogl.components.Transition;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 
 import java.awt.*;
 
 public class Night {
+    private static final Float MIDNIGHT_OPACITY = 0.5f;
+    private static final Float MIDDAY_OPACITY = 0.0f;
+
     public static GameObject create(Vector2 windowDimensions, float cycleLength) {
         GameObject night = new GameObject(Vector2.ZERO, windowDimensions, new RectangleRenderable(Color.BLACK));
         night.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         night.setTag("night");
+        new Transition<Float>(night,
+                night.renderer()::setOpaqueness,
+                MIDDAY_OPACITY,
+                MIDNIGHT_OPACITY,
+                Transition.CUBIC_INTERPOLATOR_FLOAT,
+                cycleLength / 2,
+                Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
+                null);
         return night;
     }
 
