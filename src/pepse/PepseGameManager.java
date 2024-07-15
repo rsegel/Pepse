0c77b89f09fb04c4c37c466c389fd8c685e99d5d
roise <roi.segel@mail.huji.ac.il>
2024-07-15 18:55:54 +0300

    added sky

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,16 +1,22 @@
 package pepse;
 
 import danogl.GameManager;
+import danogl.GameObject;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import pepse.world.Sky;
 
 public class PepseGameManager extends GameManager {
+
+    private static final int skyLayer = 1;
 
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        GameObject sky = Sky.create(windowController.getWindowDimensions());
+        gameObjects().addGameObject(sky, skyLayer);
     }
 
     public static void main(String[] args) {
