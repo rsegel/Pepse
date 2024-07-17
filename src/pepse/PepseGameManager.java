4dd78e70f0b7a8272e020d9feb9baefbc4fabc6a
roise <roi.segel@mail.huji.ac.il>
2024-07-17 17:13:25 +0300

    itai infinite world

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -17,6 +17,7 @@ import pepse.world.trees.Flora;
 import pepse.world.trees.Fruit;
 import pepse.world.trees.Leaf;
 import pepse.world.trees.Tree;
+import static pepse.world.Block.SIZE;
 
 import java.util.List;
 
@@ -26,14 +27,27 @@ public class PepseGameManager extends GameManager {
     private static final int NIGHT_LAYER = Layer.UI + 1; //TODO - consider
     private static final int CYCLE_DEFAULT = 30;
     private static final float OFFSIDE_AVATAR_Y = 200;
+    private static final int MIN_INIT_RANGE = 0;
+    private static final int MAX_INIT_RANGE = 1600;
+    private Vector2 windowDimensions;
+    private float minLegitX;
+    private float maxLegitX;
+    private static final int NUM_OF_BLCK_FOR_BUFFER = 5;
+    private static final float WORLD_BUFFER_FACTOR =  SIZE * NUM_OF_BLCK_FOR_BUFFER;
+    private Terrain t;
+    private Flora flora;
+    private Avatar avatar;
 
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
 //        windowController.setTargetFramerate(10);
+        windowDimensions = windowController.getWindowDimensions();
+        minLegitX = - WORLD_BUFFER_FACTOR;
+        maxLegitX = windowDimensions.x() + WORLD_BUFFER_FACTOR;
         GameObject sky = Sky.create(windowController.getWindowDimensions());
         gameObjects().addGameObject(sky, skyLayer);
-        Terrain t = new Terrain(windowController.getWindowDimensions(), 0);
+        t = new Terrain(windowController.getWindowDimensions(), 0);
         // TODO - decide if ok, needed Terrain to be before Sun
         GameObject sun = Sun.create(windowController.getWindowDimensions(), CYCLE_DEFAULT);
         gameObjects().addGameObject(sun, skyLayer);
@@ -41,29 +55,12 @@ public class PepseGameManager extends GameManager {
         gameObjects().addGameObject(night, NIGHT_LAYER);
         Vector2 avatarPositionTopLeft = new Vector2(windowController.getWindowDimensions().x()/2,
                 Terrain.getGroundHeightAtX0()-OFFSIDE_AVATAR_Y);
-        Avatar avatar = new Avatar(avatarPositionTopLeft, inputListener, imageReader);
+        avatar = new Avatar(avatarPositionTopLeft, inputListener, imageReader);
         gameObjects().addGameObject(avatar);
-        List<Block> blockList = t.createInRange(1,1600);
-        // TODO: add functionality to add only the top blocks in each column to a layer that supports collisions
-        for (Block block : blockList) {
-            if (block.getTag().equals("topLayerBlock"))
-                gameObjects().addGameObject(block, Layer.STATIC_OBJECTS); // TODO: add to correct layer
-            else gameObjects().addGameObject(block, Layer.BACKGROUND);
-        }
-        Flora flora = new Flora();
-        List<Tree> treesList = flora.createInRange(0, (int) windowController.getWindowDimensions().x());
-        for (Tree tree : treesList){
-            gameObjects().addGameObject(tree, Layer.STATIC_OBJECTS);
-            avatar.addJumpObserver(tree.avatarJumped());
-            List<Leaf> leaves = tree.getLeaves();
-            for (Leaf leaf : leaves){
-                gameObjects().addGameObject(leaf, Layer.STATIC_OBJECTS);
-            }
-            List<Fruit> fruits = tree.getFruits();
-            for (Fruit fruit : fruits){
-                gameObjects().addGameObject(fruit, Layer.STATIC_OBJECTS);
-            }
-        }
+
+        useTerrainToCreateGround(MIN_INIT_RANGE, MAX_INIT_RANGE);
+        flora = new Flora();
+        useFloraToCreateTrees(MIN_INIT_RANGE, MAX_INIT_RANGE);
         GameObject sunHalo = SunHalo.create(sun);
         gameObjects().addGameObject(sunHalo, skyLayer);
         // TODO: smart formula for the OFFSIDE_AVATAR_Y
@@ -80,7 +77,70 @@ public class PepseGameManager extends GameManager {
                 windowController.getWindowDimensions()));
     }
 
+    private void useFloraToCreateTrees(int minInitRange, int maxInitRange) {
+        List<Tree> treesList = flora.createInRange(minInitRange, maxInitRange);
+        for (Tree tree : treesList){
+            gameObjects().addGameObject(tree, Layer.STATIC_OBJECTS);
+            avatar.addJumpObserver(tree.avatarJumped());
+            List<Leaf> leaves = tree.getLeaves();
+            for (Leaf leaf : leaves){
+                gameObjects().addGameObject(leaf, Layer.STATIC_OBJECTS);
+            }
+            List<Fruit> fruits = tree.getFruits();
+            for (Fruit fruit : fruits){
+                gameObjects().addGameObject(fruit, Layer.STATIC_OBJECTS);
+            }
+        }
+    }
+
+    private void useTerrainToCreateGround(int minRange,int maxRange) {
+        List<Block> blockList = t.createInRange(minRange, maxRange);
+        // TODO: add functionality to add only the top blocks in each column to a layer that supports collisions
+        for (Block block : blockList) {
+            if (block.getTag().equals("topLayerBlock"))
+                gameObjects().addGameObject(block, Layer.STATIC_OBJECTS); // TODO: add to correct layer
+            else gameObjects().addGameObject(block, Layer.BACKGROUND);
+        }
+    }
+
     public static void main(String[] args) {
         new PepseGameManager().run();
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        infinityScrolling();
+    }
+
+    private void infinityScrolling() {
+        float maxDisplayedX = camera().screenToWorldCoords(windowDimensions).x();
+        float minDisplayedX = maxDisplayedX - windowDimensions.x();
+        if (minDisplayedX < minLegitX) {
+            changeObjsInRange((int) (minDisplayedX - WORLD_BUFFER_FACTOR), (int) minDisplayedX);
+            minLegitX = minLegitX - WORLD_BUFFER_FACTOR;
+        }
+        if (maxDisplayedX > maxLegitX) {
+            changeObjsInRange((int) maxDisplayedX, (int) (maxDisplayedX + WORLD_BUFFER_FACTOR));
+            maxLegitX = maxLegitX + WORLD_BUFFER_FACTOR;
+        }
+
+    }
+
+    private void changeObjsInRange(int min, int max) {
+        deleteNonVisibleObjects();
+        useTerrainToCreateGround((int) min, (int) max);
+        useFloraToCreateTrees((int) min, (int) max);
+    }
+
+    private void deleteNonVisibleObjects() {
+        for (GameObject gameObject : gameObjects()) {
+            if (
+                    gameObject.transform().getTopLeftCorner().x() < minLegitX - WORLD_BUFFER_FACTOR ||
+                            gameObject.transform().getTopLeftCorner().x() > maxLegitX + WORLD_BUFFER_FACTOR
+            ) {
+                gameObjects().removeGameObject(gameObject);
+            }
+        }
     }
 }
